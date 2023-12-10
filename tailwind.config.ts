401bd3f26f09c802ddfae00294e68ba202cb7cbf
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2023-12-10 15:28:14 +0530

    demo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,10 @@ const { nextui } = require("@nextui-org/react");
 /** @type {import(tailwindcss).Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
